af7ba37857ba1fe1ca4ec017e6295cf9992437e9
Desmends27 <desmondmends27@gmail.com>
2023-05-13 01:13:06 +0000

    added empyt files for tests

---
+++ tests/test_city.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Tests for  city class"""
+
+import unittest
+
+
+class Test_City(unittest.TestCase):
+    """Test for the city class"""
+    pass
---
+++ tests/test_state.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Contains test for the state class"""
+import unittest
+
+
+class State(unittest.TestCase):
+    """Test for the state class"""
+    pass
